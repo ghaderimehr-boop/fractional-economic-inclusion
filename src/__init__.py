833699870951c10abe98fe1797e51834b514a75b
Mehran Ghaderi <ghaderimehr@gmail.com>
2026-04-25 23:36:28 +0330

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+# Fractional Economic Inclusion package
+from .l1_derivative import l1_caputo_derivative
+from .composite_operator import composite_fractional_operator
+from .economic_model import g_func, delta_radius, p_t
